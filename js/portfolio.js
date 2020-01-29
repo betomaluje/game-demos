969d66996548075834c308a41b07c12833386b17
Beto Maluje <beto.maluje@gmail.com>
2020-01-29 20:48:24 +0100

    Let's try this

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,22 +1,25 @@
 $(function ($) {
 
+  var gameFrame;
+
   $('#portfolioModal1, #portfolioModal2, #portfolioModal3').on('show.bs.modal', function (event) {
     var view = $(event.relatedTarget); // view that triggered the modal    
     var projectUrl = view.data('url'); // Extract info from data-* attributes
     var id = view.data('id'); // Extract info from data-* attributes
 
-    var gameFrame = document.getElementById('unity-url' + id);
+    gameFrame = document.getElementById('unity-url' + id);
     gameFrame.src = projectUrl;
   });
 
-  $('#portfolioModal1, #portfolioModal2, #portfolioModal3').on('hidden.bs.modal', function (event) {  
-    var modal = $(this);
-    var frame = modal.find('.iframe');
+  $('#portfolioModal1, #portfolioModal2, #portfolioModal3').on('hide.bs.modal', function (event) {  
+    if (gameFrame == null) return;
+   
+    console.log('hiding modal ', gameFrame);
 
-    console.log('hiding modal ', frame);
+    gameFrame.src = 'about:blank';   
 
-    frame.src = "about:blank";
-    frame.contents().find("body").html('');
+    console.log('hiding modal 2 ', gameFrame);
+    gameFrame = null;
   });
 
   $(".button1, .button2, .button3").click(function () {
