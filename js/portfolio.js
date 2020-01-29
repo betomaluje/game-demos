8459d150153578c623a679e0a17160f1556c5bb2
Beto Maluje <beto.maluje@gmail.com>
2020-01-29 18:55:45 +0100

    UPDATE minor code refactor and identation

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,19 +1,16 @@
-$(function($) {
+$(function ($) {
 
-  $('#portfolioModal1, #portfolioModal2, #portfolioModal3').on('show.bs.modal', function (event) {  
-      var view = $(event.relatedTarget); // view that triggered the modal    
-      var projectUrl = view.data('url'); // Extract info from data-* attributes
-      console.log("url: " + projectUrl);
+  $('#portfolioModal1, #portfolioModal2, #portfolioModal3').on('show.bs.modal', function (event) {
+    var view = $(event.relatedTarget); // view that triggered the modal    
+    var projectUrl = view.data('url'); // Extract info from data-* attributes
+    var id = view.data('id'); // Extract info from data-* attributes
 
-      var id = view.data('id'); // Extract info from data-* attributes
-      console.log("id: " + id);
-
-      var gameFrame = document.getElementById('unity-url' + id);
-      gameFrame.src = projectUrl;
+    var gameFrame = document.getElementById('unity-url' + id);
+    gameFrame.src = projectUrl;
   });
 
-	$(".button1, .button2, .button3").click(function(){
-    	$("#controls1, #controls2, #controls3").toggle();
-  	});
+  $(".button1, .button2, .button3").click(function () {
+    $("#controls1, #controls2, #controls3").toggle();
+  });
 
 });
